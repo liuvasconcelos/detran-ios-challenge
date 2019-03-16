2c1fe49b65a0bd90de46e8712e7220d27e207f97
liuvasconcelos <ltenoriovasconcelos@gmail.com>
2019-03-16 15:52:07 -0300

    Initial Commit

---
+++ detran-ios-challengeTests/detran_ios_challengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  detran_ios_challengeTests.swift
+//  detran-ios-challengeTests
+//
+//  Created by Livia Vasconcelos on 16/03/19.
+//  Copyright © 2019 Livia Vasconcelos. All rights reserved.
+//
+
+import XCTest
+@testable import detran_ios_challenge
+
+class detran_ios_challengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
